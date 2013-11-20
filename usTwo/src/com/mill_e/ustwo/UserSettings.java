53d4027c5dabe17642abd1a158a5346c41fd1641
Tucker Vento <tuckervento@gmail.com>
2013-11-19 21:13:59 -0500

    (US10:TA85) Created UserSettings class, migrating applicable properties
    (US10:TA57) Added "Name" setting

--- usTwo/src/com/mill_e/ustwo/UserSettings.java
+++ usTwo/src/com/mill_e/ustwo/UserSettings.java
@@ -1,8 +1,44 @@
 package com.mill_e.ustwo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
- * Created by Owner on 11/19/13.
+ * This class contains all of the user-specified settings for UsTwo.
  */
 public class UserSettings {
 
+    private String _userName;
+    private final List<String> _defaultNameList = new ArrayList<String>();
+
+    /**
+     * Constructor for the user-specified settings object.
+     */
+    public UserSettings(){
+        generateDefaultName();
+    }
+
+    private void generateDefaultName() {
+        _defaultNameList.add("Boo-Bear");
+        _defaultNameList.add("Boo");
+        _defaultNameList.add("BB");
+        _defaultNameList.add("Babe");
+        _defaultNameList.add("Baby");
+        _defaultNameList.add("Honeydick");
+
+        _userName = _defaultNameList.get(new Random().nextInt(_defaultNameList.size()));
+    }
+
+    /**
+     * Set the username.
+     * @param p_name The username
+     */
+    public void setUserName(String p_name){ _userName = p_name; }
+
+    /**
+     * Gets the username.
+     * @return The username
+     */
+    public String getUserName(){ return _userName; }
 }
--- usTwo/src/com/mill_e/ustwo/UserSettingsFragment.java
+++ usTwo/src/com/mill_e/ustwo/UserSettingsFragment.java
@@ -3,20 +3,41 @@ package com.mill_e.ustwo;
 import android.app.Fragment;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * This fragment allows the user to specify settings.
  */
 public class UserSettingsFragment extends Fragment {
 
-    private UserSettings
+    private UserSettings _settings;
 
     private ServiceConnection _serviceConnection = new ServiceConnection() {
         @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) { _serviceRef = ((UsTwoService.UsTwoBinder) iBinder).getService(); }
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) { _settings = ((UsTwoService.UsTwoBinder) iBinder).getService().getSettings(); }
 
         @Override
-        public void onServiceDisconnected(ComponentName componentName) { _serviceRef = null; }
+        public void onServiceDisconnected(ComponentName componentName) { }
     };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        UsTwoHome.ACTIVE_FRAGMENT = 4;
+        final View v = inflater.inflate(R.layout.fragment_settings_view, container, false);
+
+        v.findViewById(R.id.button_save_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                _settings.setUserName(((EditText)v.findViewById(R.id.editText_settings_username)).getText().toString());
+            }
+        });
+        ((EditText)v.findViewById(R.id.editText_settings_username)).setText(_settings.getUserName());
+
+        return v;
+    }
 }
